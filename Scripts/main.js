72e5d39f643f7cc9478a0287df64c006d25cbaa0
Sypnotick <58993512+Sypnotick@users.noreply.github.com>
2021-11-03 16:16:32 +0000

    Main

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,8 +1,13 @@
-const shareBox = document.getElementById('card-share-option');
-const shareButton = document.getElementById("card-user-share");
+let viewportHeight = window.innerHeight;
+let viewportWidth = window.innerWidth;
 
-shareButton.addEventListener('click', () => {
-    console.log("click!");
+
+const shareBox = document.getElementById('card-share-option');
+const shareButton = document.querySelectorAll('.card-user-share');
+const userInfo = document.getElementById('user-box');
+
+shareButton.forEach((el) =>
+  el.addEventListener('click', () => {
 
     if(shareBox.style.display === "none")
     {
@@ -11,4 +16,18 @@ shareButton.addEventListener('click', () => {
     else {
         shareBox.style.display = "none";
     }
-});
+
+    if(viewportWidth <= 400) {
+
+        if(userInfo.style.display !== "none") {
+            userInfo.style.display = "none";       
+        }
+        else {
+            userInfo.style.display = "flex";
+        }
+    }
+  }) 
+);
+
+
+
